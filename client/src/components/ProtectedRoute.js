2ffddc5663163ddeb0667bf062cd3a331d1bcb0e
farxa <r.abualhassan@gmail.com>
2023-09-28 22:51:09 +0200

    clean repo and start anew

--- client/src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({children, redirectTo, token}) {
-    
-  return token ? children : <Navigate to={redirectTo} />
-}
